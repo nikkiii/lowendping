a6e8704993133a6f2a4e964c1bf2220f7aac523c
nikkiii <nikki@nikkii.us>
2014-03-09 09:41:35 -0400

    Add example server and empty database

--- app/config/lowendping.php
+++ app/config/lowendping.php
@@ -3,8 +3,6 @@
 return array(
 		
 	'servers' => array(
-		1 => array('name' => 'QuickPacket - Atlanta, GA', 'host' => '199.233.232.52', 'port' => 12337),
-		2 => array('name' => 'BuyVM - Las Vegas, Nevada', 'host' => '199.19.227.51', 'port' => 12337),
-		3 => array('name' => 'BuyVM - New Jersey', 'host' => '199.195.250.46', 'port' => 12337)
+		1 => array('name' => 'Example', 'host' => 'localhost', 'port' => 12337)
 	)
 );
