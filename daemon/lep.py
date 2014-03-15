39e6866d27f9f9f799752353d4449f689e65d48d
nikkiii <nikki@nikkii.us>
2014-03-15 16:18:35 -0400

    Updated and (hopefully) more secure daemon

--- daemon/lep.py
+++ daemon/lep.py
@@ -3,9 +3,10 @@ import Queue
 import json
 import SocketServer
 import socket
+import ipaddress
 from time import sleep
 
-from sh import ping, ping6, traceroute
+from sh import ping, ping6, traceroute, mtr
 
 try:
 	import lepconf
@@ -15,6 +16,12 @@ except ImportError, err:
 
 ping = ping.bake(c=4, _ok_code=[0,1])
 ping6 = ping6.bake(c=4, _ok_code=[0,1])
+
+traceroute4 = traceroute.bake("-4")
+traceroute6 = traceroute.bake("-6")
+
+mtr4 = mtr.bake("-4", "--report", "--report-wide")
+mtr6 = mtr.bake("-6", "--report", "--report-wide")
 
 queue = Queue.Queue()
 
@@ -26,14 +33,25 @@ class QueryThread(threading.Thread):
 	def run(self):
 		while True:
 			query = self.queue.get()
+			out = None
 			if query.type == "ping":
 				out = ping(query.query)
 			elif query.type == "ping6":
 				out = ping6(query.query)
 			elif query.type == "trace":
-				out = traceroute(query.query)
-			send_response(query, out)
-			print "Processed query",str(query.id)
+				out = traceroute4(query.query)
+			elif query.type == "trace6":
+				out = traceroute6(query.query)
+			elif query.type == "mtr":
+				out = mtr4(query.query)
+			elif query.type == "mtr6":
+				out = mtr6(query.query)
+			
+			if out:
+				print "Processed query",str(query.id)
+				send_response(query, out)
+			else:
+				print "Unable to process query",str(query.id),"- invalid type",type
 
 class Query:
 	def __init__(self, id, serverid, query, type):
@@ -46,11 +64,28 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 	def handle(self):
 		data = self.request.recv(1024)
 		obj = json.loads(data)
-		if obj['auth'] == lepconf.auth:
+		
+		if not obj['auth'] == lepconf.auth:
+			put(self.request, 'denied')
+		elif not valid_query(obj['query']):
+			put(self.request, 'invalid_query')
+		else:
 			queue.put(Query(obj['id'], obj['serverid'], obj['query'], obj['type']))
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 	pass
+
+def valid_query(query):
+	try:
+		if ipaddress.ip_address(query):
+			return 1
+	except ValueError:
+		pass
+	try:
+		socket.gethostbyname(query)
+		return 1
+	except socket.error:
+		return 0
 
 def send_response(query, response):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
